2a13cdb5172e494a78bfaff76d6b208c000107fa
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2026-01-10 22:39:53 +0900

    update

--- nvim/lua/Sethy/plugins/snacks.lua
+++ nvim/lua/Sethy/plugins/snacks.lua
@@ -1,155 +1,157 @@
 return {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-        -- HACK: docs @ https://github.com/folke/snacks.nvim/blob/main/docs
-        explorer = {
-            enabled = true,
-            layout = {
-                cycle = false,
-            },
-        },
-        quickfile = {
-            enabled = true,
-            exclude = {"latex"},
-        },
-        -- HACK: read picker docs @ https://github.com/folke/snakcs.nvim/blob/main/docs/picker.md
-        picker = {
-            enabled = true,
-            matchers = {
-                frecency = true,
-                cwd_bonus = true,
-
-            },
-            formatters = {
-                file = {
-                    filename_first = false,
-                    filename_only = false,
-                    icon_width = 2,
-                }
-            },
-            layout = {
-                preset = "telescope",
-                cycle = false,
-            },
-            layouts = {
-                select = {
-                    preview = false,
-                    layout = {
-                        backdrop = false,
-                        width = 0.6,
-                        min_width = 80,
-                        heighk = 0.4,
-                        min_height = 10,
-                        box = "vertical",
-                        border = "rounded",
-                        title = "{title}",
-                        title_pos = "center",
-                        { win = "input", height = 1, border = "bottom" },
-                        { win = "list", border = "none" },
-                        { win = "preview", title = "{preview}", width = 0.6, height = 0.4, border = "top" },
-                    },
+    -- HACK: docs @ https://github.com/folke/snacks.nvim/blob/main/docs
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        opts = {
+            explorer = {
+                enabled = true,
+                layout = {
+                    cycle = false,
                 },
-                telescope = {
-                    reverse = true, -- set to false for search bar to be on top
-                    layout = {
-                        box = "horizontal",
-                        backdrop = false,
-                        width = 0.8,
-                        height = 0.9,
-                        border = "none",
-                        {
-                            box = "vertical",
-                            { win = "list", title = "results", title_pos = "center", border = "rounded" },
-                            { win = "input", height = 1, border = "rounded", title = "{title} {live} {flags}", title_pos = "center" },
-                        },
-                        {
-                            win = "preview",
-                            title = "{preview:preview}",
-                            width = 0.50,
-                            border = "rounded",
-                            title_pos = "center",
-                        }
+            },
+            quickfile = {
+                enabled = true,
+                exclude = {"latex"},
+            },
+            -- HACK: read picker docs @ https://github.com/folke/snakcs.nvim/blob/main/docs/picker.md
+            picker = {
+                enabled = true,
+                matchers = {
+                    frecency = true,
+                    cwd_bonus = true,
+
+                },
+                formatters = {
+                    file = {
+                        filename_first = false,
+                        filename_only = false,
+                        icon_width = 2,
                     }
                 },
-                ivy = {
-                    layout = {
-                        box = "vertical",
-                        backdrop = false,
-                        width = 0,
-                        height = 0.4,
-                        position = "bottom",
-                        border = "top",
-                        title = "{title} {live} {flags}",
-                        title_pos = "left",
-                        { win = "input", height = 1, border = "bottom" },
-                        {
+                layout = {
+                    preset = "telescope",
+                    cycle = false,
+                },
+                layouts = {
+                    select = {
+                        preview = false,
+                        layout = {
+                            backdrop = false,
+                            width = 0.6,
+                            min_width = 80,
+                            height = 0.4,
+                            min_height = 10,
+                            box = "vertical",
+                            border = "rounded",
+                            title = "{title}",
+                            title_pos = "center",
+                            -- { win = "input", height = 1, border = "bottom" },
+                            -- { win = "list", border = "none" },
+                            -- { win = "preview", title = "{preview}", width = 0.6, height = 0.4, border = "top" },
+                        },
+                    },
+                    telescope = {
+                        reverse = true, -- set to false for search bar to be on top
+                        layout = {
                             box = "horizontal",
-                            { win = "list", border = "none" },
-                            { win = "preview", title = "{preview}", width = 0.5, border = "left" },
+                            backdrop = false,
+                            width = 0.8,
+                            height = 0.9,
+                            border = "none",
+                            {
+                                box = "vertical",
+                                { win = "list", title = "results", title_pos = "center", border = "rounded" },
+                                { win = "input", height = 1, border = "rounded", title = "{title} {live} {flags}", title_pos = "center" },
+                            },
+                            {
+                                win = "preview",
+                                title = "{preview:preview}",
+                                width = 0.50,
+                                border = "rounded",
+                                title_pos = "center",
+                            }
+                        }
+                    },
+                    ivy = {
+                        layout = {
+                            box = "vertical",
+                            backdrop = false,
+                            width = 0,
+                            height = 0.4,
+                            position = "bottom",
+                            border = "top",
+                            title = "{title} {live} {flags}",
+                            title_pos = "left",
+                            { win = "input", height = 1, border = "bottom" },
+                            {
+                                box = "horizontal",
+                                { win = "list", border = "none" },
+                                { win = "preview", title = "{preview}", width = 0.5, border = "left" },
+                            },
                         },
                     },
                 },
             },
-        },
-        -- image  = {
-        --     enabled = true,
-        --     doc = {
-        --         float = false,
-        --         inline = true, -- if you want show image on cursor hover
-        --         max_width = 50,
-        --         wo = {
-        --             wrap = true,
-        --         },
-        --         convert = {
-        --             notify = true,
-        --             command = "magick"
-        --         },
-        --         img_dirs = { "img", "images", "assets", "static", "public", "media", "attachments", "Archives/All-Vault-Images/", "~/Library", "~/Downloads" },
-        --     },
-        -- },
-        dashboard = {
-            enabled = true,
-            sections = {
-                { section = "header" },
-                { section = "keys", gap = 1, padding = 1 },
-                {
-                    section = "terminal",
-                    cmd = ("ascii-image-converter %q -C -c")
-                        :format(vim.fn.expand("~/Documents/doraemon.JPG")),
-                    pane = 2,
-                    indent = 4,
-                    height = 30,
+            -- image  = {
+            --     enabled = true,
+            --     doc = {
+            --         float = false,
+            --         inline = true, -- if you want show image on cursor hover
+            --         max_width = 50,
+            --         wo = {
+            --             wrap = true,
+            --         },
+            --         convert = {
+            --             notify = true,
+            --             command = "magick"
+            --         },
+            --         img_dirs = { "img", "images", "assets", "static", "public", "media", "attachments", "Archives/All-Vault-Images/", "~/Library", "~/Downloads" },
+            --     },
+            -- },
+            dashboard = {
+                enabled = true,
+                sections = {
+                    { section = "header" },
+                    { section = "keys", gap = 1, padding = 1 },
+                    {
+                        section = "terminal",
+                        cmd = ("ascii-image-converter %q -C -c")
+                            :format(vim.fn.expand("~/Documents/doraemon.JPG")),
+                        pane = 2,
+                        indent = 4,
+                        height = 30,
+                    },
                 },
             },
         },
-    },
-    keys = {
-        { "<leader>lg", function() require("snacks").lazygit() end, desc = "lazygit" },
-        { "<leader>gl", function() require("snacks").lazygit.log() end, desc = "lazygit logs" },
-        { "<leader>es", function() require("snacks").explorer() end, desc = "open snacks explorer" },
-        { "<leader>rn", function() require("snacks").rename.rename_file() end, desc = "fast rename current file" },
-        { "<leader>db", function() require("snacks").bufdelete() end, desc = "delete or close buffer (confirm)" },
+        keys = {
+            { "<leader>lg", function() require("snacks").lazygit() end, desc = "lazygit" },
+            { "<leader>gl", function() require("snacks").lazygit.log() end, desc = "lazygit logs" },
+            { "<leader>es", function() require("snacks").explorer() end, desc = "open snacks explorer" },
+            { "<leader>rn", function() require("snacks").rename.rename_file() end, desc = "fast rename current file" },
+            { "<leader>db", function() require("snacks").bufdelete() end, desc = "delete or close buffer (confirm)" },
 
 
-        -- snacks picker
-        { "<leader>pf", function() require("snacks").picker.files() end, desc = "find files (snacks picker)" },
-        { "<leader>pc", function() require("snacks").picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "find config file" },
-        { "<leader>ps", function() require("snacks").picker.grep() end, desc = "grep word" },
-        { "<leader>pws", function() require("snacks").picker.grep_word() end, desc = "search visual selection or word", mode = { "n", "x" } },
-        { "<leader>pk", function() require("snacks").picker.keymaps({layout = "ivy" }) end, desc = "search keymaps (snacks picker)" },
-        
-        { "<leader>gbr", function() require("snacks").picker.git_branches({ layout = "select" }) end, desc = "pick and switch git branches" },
-        { "<leader>th", function() require("snacks").picker.colorschemes({ layout = "ivy" }) end, desc = "pick color schemes" },
-        { "<leader>vh", function() require("snacks").picker.help() end, desc = "help pages" },
+            -- snacks picker
+            { "<leader>pf", function() require("snacks").picker.files() end, desc = "find files (snacks picker)" },
+            { "<leader>pc", function() require("snacks").picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "find config file" },
+            { "<leader>ps", function() require("snacks").picker.grep() end, desc = "grep word" },
+            { "<leader>pws", function() require("snacks").picker.grep_word() end, desc = "search visual selection or word", mode = { "n", "x" } },
+            { "<leader>pk", function() require("snacks").picker.keymaps({layout = "ivy" }) end, desc = "search keymaps (snacks picker)" },
+            
+            { "<leader>gbr", function() require("snacks").picker.git_branches({ layout = "select" }) end, desc = "pick and switch git branches" },
+            { "<leader>th", function() require("snacks").picker.colorschemes({ layout = "ivy" }) end, desc = "pick color schemes" },
+            { "<leader>vh", function() require("snacks").picker.help() end, desc = "help pages" },
+        },
     },
     {
         "folke/todo-comments.nvim",
         event = { "BufReadPre", "BufNewFile" },
         keys = {
-            { "<leader>pt", function() require("snacks").picker.todo_comments() end, desc = "Todo" },
-            { "<leader>pT", function() require("snacks").picker.todo_commens({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+            { "<leader>pt", function() require("snacks").picker.todo_comments() end, desc = "todo" },
+            { "<leader>pT", function() require("snacks").picker.todo_comments({ keywords = { "todo", "fix", "fixme" } }) end, desc = "todo/fix/fixme" },
         },
     },
 }
