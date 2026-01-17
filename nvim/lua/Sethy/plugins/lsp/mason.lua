43bd36600efb419a6721783948ff41393f536154
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2026-01-17 21:21:25 +0900

    update mason

--- nvim/lua/Sethy/plugins/lsp/mason.lua
+++ nvim/lua/Sethy/plugins/lsp/mason.lua
@@ -14,9 +14,11 @@ return {
         local mason_lspconfig = require("mason-lspconfig")
         local mason_tool_installer = require("mason-tool-installer")
 
-        local lspconfig = require("lspconfig")
-        local cmp_nvim_lsp = require("cmp_nvim_lsp")
-        local capabilities = cmp_nvim_lsp.default_capabilities() -- used to enable autocompletion
+        -- NOTE: Moved these local imports below back to lspconfig.lua due to mason depracated handlers
+
+        -- local lspconfig = require("lspconfig")
+        -- local cmp_nvim_lsp = require("cmp_nvim_lsp")             -- import cmp-nvim-lsp plugin
+        -- local capabilities = cmp_nvim_lsp.default_capabilities() -- used to enable autocompletion (assign to every lsp server config)
 
         -- enable mason and configure icons
         mason.setup({
@@ -28,126 +30,21 @@ return {
                 },
             },
         })
+
         mason_lspconfig.setup({
-            --servers for mason to install
+            automatic_enable = false,
+            -- servers for mason to install
             ensure_installed = {
                 "lua_ls",
-                -- "ts_ls", currently using a ts plugin
+                "ts_ls",
                 "html",
                 "cssls",
                 "tailwindcss",
                 "gopls",
+                "astro",
                 "emmet_ls",
                 "emmet_language_server",
-                -- "eslint",
-                "marksman",
-            },
-            -- auto install configured servers (with lspconfig)
-            automatic_installation = true,
-            -- handlers (replaces deprecated setup_handlers)
-            handlers = {
-                -- default handler for installed servers
-                function(server_name)
-                    lspconfig[server_name].setup({
-                        capabilities = capabilities,
-                    })
-                end,
-                ["emmet_ls"] = function()
-                    -- configure emmet language server
-                    lspconfig["emmet_ls"].setup({
-                        capabilities = capabilities,
-                        filetypes = {
-                            "html",
-                            "typescriptreact",
-                            "javascript",
-                            "css",
-                            "sass",
-                            "scss",
-                            "less",
-                            "svelte",
-                        },
-                    })
-                end,
-                ["lua_ls"] = function()
-                    -- configure lua server (with special settings)
-                    lspconfig["lua_ls"].setup({
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                -- make the language server recognize "vim" global
-                                diagnostics = {
-                                    globals = { "vim" },
-                                },
-                                completion = {
-                                    callSnippet = "Replace",
-                                },
-                                workspace = {
-                                    -- make language server aware of run time files
-                                    library = {
-                                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                                        [vim.fn.stdpath("config") .. "/lua"] = true,
-                                    },
-                                },
-                            },
-                        },
-                    })
-                end,
-                ["emmet_language_server"] = function()
-                    lspconfig.emmet_language_server.setup({
-                        filetypes = {
-                            "css",
-                            "eruby",
-                            "html",
-                            "javascript",
-                            "javascriptreact",
-                            "less",
-                            "sass",
-                            "scss",
-                            "pug",
-                            "typescript",
-                        },
-                        -- Read more about this options the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
-                        -- **Note:** only the options listed in the table are supported.
-                        init_options = {
-                            -- @type table<string, string>
-                            includeLanguages = {},
-                            --- @type string[]
-                            excludeLanguage = {},
-                            --- @type string[]
-                            extensionPath = {},
-                            --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
-                            preferences = {},
-                            --- @type boolean Defaults to `true`
-                            showAbbreviationSuggestions = true,
-                            --- @type "always" | "never" Defaults to `"always"`
-                            showExpandedAbbreviation = "always",
-                            --- @type boolean Defaults to `false`
-                            showSuggestionsAsSnippets = false,
-                            --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
-                            syntaxProfiles = {},
-                            --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
-                            variables = {},
-                        },
-                    })
-                end,
-                ["denols"] = function()
-                    lspconfig.denols.setup({
-                        capabilities = capabilities,
-                        root_dir = function(fname)
-                            -- Use tsserver unless a Deno-specific is present
-                            local util = lspconfig.util
-                            return not util.root_pattern('deno.json', 'deno.jsonc')(fname)
-                                and util.root_pattern('tsconfig.json', 'package.json', 'jsconfig.json', 'git')(fname)
-                        end,
-                        single_file_support = false,
-                        init_options = {
-                            preferences = {
-                                includeCompletionsWithSnippetText = true,
-                                includeCompletionsForImportStatesments = true,
-                            },
-                        },
-                    })
-                end,
+                "eslint",
             },
         })
 
@@ -158,9 +55,12 @@ return {
                 "isort",    -- python formatter
                 "pylint",
                 "clangd",
-                "deno",
-                { 'eslint_d', version = '13.1.1' },
+                "denols",
+                -- { 'eslint_d', version = '13.1.2' },
             },
+
+            -- NOTE: mason BREAKING Change! Removed setup_handlers
+            -- moved lsp configuration settings back into lspconfig.lua file
         })
-    end
+    end,
 }
