81df61071527d60efca6accefc7dc9820a408673
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2026-01-23 03:09:56 +0900

    update

--- nvim/lua/Sethy/plugins/snacks.lua
+++ nvim/lua/Sethy/plugins/snacks.lua
@@ -239,6 +239,8 @@ return {
 	{
 		"folke/todo-comments.nvim",
 		event = { "BufReadPre", "BufNewFile" },
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
 		keys = {
 			{
 				"<leader>pt",
