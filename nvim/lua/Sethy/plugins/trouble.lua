4baedbcc3e1057ea9046481a9be723eb211ef5a0
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2026-01-28 23:54:12 +0900

    fic typo

--- nvim/lua/Sethy/plugins/trouble.lua
+++ nvim/lua/Sethy/plugins/trouble.lua
@@ -1,7 +1,7 @@
 return {
     "folke/trouble.nvim",
     dependencies = {
-        "nvim-trree/nvim-web-devicons",
+        "nvim-tree/nvim-web-devicons",
         "folke/todo-comments.nvim",
     },
     opts = {
