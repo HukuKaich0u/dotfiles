044e424435acc9717a259a9f9b3cb28fa513f14d
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2026-02-19 09:17:10 +0900

    add whitespace marker and alert

--- nvim/lua/Sethy/core/options.lua
+++ nvim/lua/Sethy/core/options.lua
@@ -54,3 +54,31 @@ vim.opt.hlsearch = true
 
 vim.opt.mouse = "a"
 vim.g.editorconfig = true
+
+-- Whitespace visibility
+vim.opt.list = false
+vim.opt.listchars = {
+    tab = ">-",
+    trail = "~",
+    nbsp = "+",
+    extends = ">",
+    precedes = "<",
+}
+
+-- Highlight suspicious whitespace (trailing spaces, spaces before tabs)
+vim.api.nvim_set_hl(0, "Whitespace", { fg = "#666666", bg = "NONE" })
+vim.api.nvim_set_hl(0, "NonText", { fg = "#666666", bg = "NONE" })
+vim.api.nvim_set_hl(0, "SpecialKey", { fg = "#666666", bg = "NONE" })
+vim.api.nvim_set_hl(0, "ExtraWhitespace", { bg = "#51202A" })
+vim.cmd([[
+  augroup SethyWhitespace
+    autocmd!
+    autocmd ColorScheme * highlight Whitespace guifg=#666666 guibg=NONE ctermfg=243 ctermbg=NONE
+    autocmd ColorScheme * highlight NonText guifg=#666666 guibg=NONE ctermfg=243 ctermbg=NONE
+    autocmd ColorScheme * highlight SpecialKey guifg=#666666 guibg=NONE ctermfg=243 ctermbg=NONE
+    autocmd ColorScheme * highlight ExtraWhitespace guibg=#51202A ctermbg=52
+    autocmd BufWinEnter,WinEnter * match ExtraWhitespace /\s\+$\| \+\ze\t/
+    autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$\| \+\ze\t/
+    autocmd InsertLeave * match ExtraWhitespace /\s\+$\| \+\ze\t/
+  augroup END
+]])
