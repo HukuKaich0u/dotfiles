49639c466ef3914fd1b81a899d0a448d65ecffc1
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2026-02-24 08:52:32 +0900

    update(keybind): setting ctrl+q as a leader of wezterm from ctrl+;

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -97,6 +97,6 @@ end)
 config.keys = require("keybinds").keys
 config.key_tables = require("keybinds").key_tables
 config.disable_default_key_bindings = true
-config.leader = { key = ";", mods = "CTRL", timeout_milliseconds = 2000 }
+config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
 
 return config
