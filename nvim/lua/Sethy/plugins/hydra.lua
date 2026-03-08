dd18dbc6a06abb3b53ada2c01840c1faa8b25b07
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2026-03-08 23:07:45 +0900

    add hydra plugin into nvim plugins

---
+++ nvim/lua/Sethy/plugins/hydra.lua
@@ -0,0 +1,36 @@
+return {
+    {
+        "anuvyklack/hydra.nvim",
+        keys = {
+            { "<leader>sr", desc = "Resize split mode" },
+        },
+        config = function()
+            local Hydra = require("hydra")
+
+            Hydra({
+                name = "Resize split",
+                mode = "n",
+                body = "<leader>sr",
+                config = {
+                    color = "pink",
+                    invoke_on_body = true,
+                    hint = [[
+     Resize split
+    _h_: narrower   _l_: wider
+    _j_: taller     _k_: shorter
+
+    _<Enter>_/_<Esc>_: exit
+                    ]],
+                },
+                heads = {
+                    { "h", "<cmd>vertical resize -5<CR>", { desc = "narrower" } },
+                    { "l", "<cmd>vertical resize +5<CR>", { desc = "wider" } },
+                    { "j", "<cmd>resize +3<CR>", { desc = "taller" } },
+                    { "k", "<cmd>resize -3<CR>", { desc = "shorter" } },
+                    { "<Enter>", nil, { exit = true, desc = "exit" } },
+                    { "<Esc>", nil, { exit = true, desc = false } },
+                },
+            })
+        end,
+    },
+}
