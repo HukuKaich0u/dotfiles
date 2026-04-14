db1cef08112b2c6cbc44df6decbfe3b9c538d636
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2026-04-14 11:35:33 +0900

    feat(nvim): use bacon for rust checks

--- .config/nvim/lua/Sethy/plugins/lsp/rustaceanvim.lua
+++ .config/nvim/lua/Sethy/plugins/lsp/rustaceanvim.lua
@@ -3,14 +3,36 @@ return {
     version = "^6",
     lazy = false,
     init = function()
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "rust",
+            callback = function(args)
+                vim.keymap.set("n", "<leader>lb", function()
+                    if vim.fn.executable("bacon") == 0 then
+                        vim.notify("bacon is not installed", vim.log.levels.ERROR)
+                        return
+                    end
+
+                    local root = vim.fs.root(args.buf, { "Cargo.toml", ".git" }) or vim.uv.cwd()
+                    Snacks.terminal("bacon", {
+                        cwd = root,
+                        win = {
+                            position = "bottom",
+                            height = 0.35,
+                        },
+                    })
+                end, {
+                    buffer = args.buf,
+                    desc = "Toggle bacon",
+                    silent = true,
+                })
+            end,
+        })
+
         vim.g.rustaceanvim = {
             server = {
                 default_settings = {
                     ["rust-analyzer"] = {
-                        checkOnSave = true,
-                        check = {
-                            command = "clippy",
-                        },
+                        checkOnSave = false,
                         cargo = {
                             allFeatures = true,
                         },
