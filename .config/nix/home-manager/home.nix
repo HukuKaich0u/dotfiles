175794852b085f5ec5d7308390bd2f8f143ae18c
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2026-04-25 23:16:37 +0900

    chore(nix): delete useless comment in home.nix

--- .config/nix/home-manager/home.nix
+++ .config/nix/home-manager/home.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "KokiAoyagi";
   home.homeDirectory = "/Users/KokiAoyagi";
 
@@ -15,29 +13,14 @@
   # release notes.
   home.stateVersion = "25.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     git
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     ".gitconfig".source = ./git/.gitconfig;
     ".gitconfig-personal".source = ./git/.gitconfig-personal;
     ".gitconfig-university".source = ./git/.gitconfig-university;
-
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   # Home Manager can also manage your environment variables through
@@ -60,6 +43,5 @@
     # EDITOR = "emacs";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
