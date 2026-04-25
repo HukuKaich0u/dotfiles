50ada6ab6bfeda2d9bc7951800f13e246f64170e
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2026-04-25 14:40:38 +0900

    feat(nix): manage git config with home-manager

--- .config/nix/home-manager/home.nix
+++ .config/nix/home-manager/home.nix
@@ -18,11 +18,16 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    git
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".gitconfig".source = ./git/.gitconfig;
+    ".gitconfig-personal".source = ./git/.gitconfig-personal;
+    ".gitconfig-university".source = ./git/.gitconfig-university;
+
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
