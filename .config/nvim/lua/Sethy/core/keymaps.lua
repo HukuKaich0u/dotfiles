e84d8c77185065df20cda8f8d335bb4c0ed0beac
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2026-04-27 23:47:44 +0900

    feat(nvim): set g<C-l> as a reload command of nvim

--- .config/nvim/lua/Sethy/core/keymaps.lua
+++ .config/nvim/lua/Sethy/core/keymaps.lua
@@ -54,6 +54,7 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to lower split" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper split" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+vim.keymap.set("n", "g<C-l>", "<cmd>redraw!<CR>", { desc = "Redraw screen" })
 
 local function map_terminal_window_nav(bufnr)
     local directions = {
