7e5109a588522feb57a8bf07023d32379ff75598
HukuKaich0u <170926658+HukuKaich0u@users.noreply.github.com>
2026-04-28 13:37:58 +0900

    feat(nix): add nix-darwin setup

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -8,24 +8,34 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs =
-    { nixpkgs, home-manager, ... }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      homeConfigurations."KokiAoyagi" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+  outputs = {
+    nixpkgs,
+    home-manager,
+    nix-darwin,
+    ...
+  }: let
+    system = "aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeConfigurations."KokiAoyagi" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home-manager/home.nix ];
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [./home-manager/home.nix];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
     };
+
+    darwinConfigurations."aoyagikoukinoMacBook-Air" = nix-darwin.lib.darwinSystem {
+      modules = [./nix-darwin/configuration.nix];
+    };
+  };
 }
---
+++ .config/nix/nix-darwin/configuration.nix
@@ -0,0 +1,5 @@
+{...}: {
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  system.stateVersion = 6;
+  nix.enable = false;
+}
